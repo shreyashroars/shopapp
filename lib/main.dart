8e1d47d8fb337eaffae65ebde19e4226be046a34
Shreyash Pandey <shreyash.edu@gmail.com>
2022-05-31 19:06:11 +0530

    added providers

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,26 @@
 import 'package:flutter/material.dart';
 import 'package:shopapp/screens/products_overview_screens.dart';
 import 'package:shopapp/widgets/product_detail_screen.dart';
+import 'package:provider/provider.dart';
+import './providers/products.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'MyShop',
-        theme: ThemeData(
-            primarySwatch: Colors.deepPurple,
-            accentColor: Colors.orange,
-            fontFamily: 'Lato'),
-        home: ProductOverview(),
-        routes: {
-          ProductDetailScreen.routename: (ctx) => ProductDetailScreen(),
-        });
+    return ChangeNotifierProvider(
+      create: (ctx) => Products(),
+      child: MaterialApp(
+          title: 'MyShop',
+          theme: ThemeData(
+              primarySwatch: Colors.deepPurple,
+              accentColor: Colors.orange,
+              fontFamily: 'Lato'),
+          home: ProductOverview(),
+          routes: {
+            ProductDetailScreen.routename: (ctx) => ProductDetailScreen(),
+          }),
+    );
   }
 }
---
+++ lib/widgets/products_grid.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:shopapp/widgets/product_item.dart';
+import '../providers/products.dart';
+import 'package:provider/provider.dart';
+
+class ProductsGrid extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final productsData = Provider.of<Products>(context);
+    final products = productsData.items;
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
+      itemCount: products.length,
+      // SliverGridDelegateWithFixedCrossAxisCount this tells the number of columns u want Here crossAxisCount=2 therefore 2 columns are set
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+          crossAxisCount: 2,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 10,
+          mainAxisSpacing: 10),
+      itemBuilder: (ctx, i) =>
+          ProductItem(products[i].id, products[i].title, products[i].imageUrl),
+    );
+  }
+}
